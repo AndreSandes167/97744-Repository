2f98f1fc09c9671bd33c51f69c454c8c4139421f
AndreSandes167 <AndreSandes250@gmail.com>
2025-03-14 18:28:07 +0000

    salvar

--- 1. for_exemplo.py
+++ 1. for_exemplo.py
@@ -4,9 +4,9 @@ os.system(" cls || clear")
 
 print("com laço de repetição")
 print()
-for i in range(100,1,2):
+for i in range(1,21,2):
  print(f"o valor da variavel é {i}")
  
- time.sleep(0.001)
+ time.sleep(0.08)
 print()
 print("Acabou!!!")
---
+++ 19.Atividade.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+os.system("clear") # limpa o terminal.
+
+
+for i in range(1, 6):
+    num = int("digite 1 numero:"))
+    soma =  num * i 
+    time.sleep(0.08)
+
+print(f"result: {soma}")
