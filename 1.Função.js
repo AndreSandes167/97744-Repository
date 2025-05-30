9928a83035c300b3f66b11a2b26da7666adf46f1
AndreSandes167 <AndreSandes250@gmail.com>
2025-05-30 19:49:21 +0000

    salvar

---
+++ 1.Função.js
@@ -0,0 +1,18 @@
+// Função.
+function somar(a, b) {
+    return a + b
+}
+
+// Função.
+function subtrair(a, b) { 
+    return a - b
+}
+
+// Chamando a função.
+const soma = somar(2,3)
+const subtração = subtrair(2,3)
+
+// Exibindo resultado.
+console.log('soma: ${soma}')
+console.log('subtração: ${subtração}')
+
---
+++ 2.Arrow Funtions.js
@@ -0,0 +1,17 @@
+// Função.
+const somar = (a, b) => { 
+    return a + b
+}
+
+// Função.
+const subtrair = (a, b) => a - b
+
+// Chamando a função.
+const soma = somar(2,3.666)
+const subtração = subtrair(2,3)
+
+// Exibindo resultado.
+console.log('soma: ${soma}`)
+console.log(`subtraçao: ${subtraçao}`)
+console.log(`Multiplicaçao: ${Multiplicaçao}`)
+console.log(`divisao: ${divisao}`)')
